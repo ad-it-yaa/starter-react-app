cdd78f491da35034b27e71675cc8431e56f4a624
Aditya <Aditya.suriyanarayanan@rbs.co.uk>
2022-12-11 00:54:11 +0530

    updates

--- src/Components/Dashboard/Header/FormHeaders.js
+++ src/Components/Dashboard/Header/FormHeaders.js
@@ -21,7 +21,7 @@ export default function FormHeaders() {
 
     <div className='form_header'>        
     <img className='form_logo' src='https://seeklogo.com/images/N/nat-west-1969-to-2016-logo-A61F210017-seeklogo.com.png' height={35}></img>
-    <div onClick={delete_token}><i className="fas fa-power-off power_button red_colo"></i></div>
+    <div onClick={delete_token}><i className="fas fa-power-off power_button red_colo mvz"></i></div>
     <div className='nav_diva'><NavLink to="/dashboard/moneytransfer"  className='nav_menu active pd'>Money Transfer</NavLink></div>
     <div className='nav_diva'><NavLink to="/dashboard/moviebooking"  className='nav_menu active pd'>Movie Booking</NavLink></div>
     <div className='nav_diva'><NavLink to="/dashboard/merchantpayment"  className='nav_menu active pd'>Merchant Payment</NavLink></div>
