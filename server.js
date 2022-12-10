b9dfe03a810010eae3125da4912ceb3afc0bfaa7
Aditya <Aditya.suriyanarayanan@rbs.co.uk>
2022-12-10 17:22:35 +0530

    Commit

--- server.js
+++ server.js
@@ -1,39 +1,16 @@
-
-const express = require('express')
-const path = require("path");
-const app = express()
-
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('build', options))
-
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-  console.log(`React app listening at http://localhost:${port}`)
-})
-
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults({
-  static: './build'
+const app = jsonServer.create();
+const path = require('path');
+const express = require('express');
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
+const port = process.env.PORT || 3001;
+
+app.use('/db', middlewares, router);
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-const PORT = process.env.PORT || 3001;
-server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}))
-server.use(router);
-server.listen(PORT, () => {
-  console.log('Server is running');
-});
+
+server.listen(port);
